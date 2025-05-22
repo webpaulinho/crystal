2ae98e08e03521d0f23b2fc87d292250e2482ff9
webpaulinho <148885790+webpaulinho@users.noreply.github.com>
2025-05-22 17:55:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -204,4 +204,4 @@ def vacation_settings(email):
             return jsonify({"error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
